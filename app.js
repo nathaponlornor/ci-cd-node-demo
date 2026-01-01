07de964982aec783e1928bda9c6251efea97f045
nathapua <nathapua@nathapuas-MacBook-Air.local>
2026-01-01 22:14:59 +0700

    update new version

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline 🚀');
+  res.send('version 2 of Hello from CI/CD Pipeline 🚀');
 });
 
 app.listen(3000, () => {
